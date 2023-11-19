3310234dc4ea317b5adaf15018be07c3b51340c6
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2023-11-19 10:24:35 +0000

    Add render to allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-v99@ke6vy&d9x6=r2gn!!9*%r!u1n&%^jcu!pb52!#0va8#1&z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangotodo-6pzv.onrender.com']
 
 
 # Application definition
